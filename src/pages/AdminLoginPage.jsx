9a783390be2810621fecb4da410a47688bfe4a58
Billiamin Abbas <abbasbilliamin2@gmail.com>
2023-08-11 11:50:46 +0100

    first-commit

---
+++ src/pages/AdminLoginPage.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import MkdSDK from "../utils/MkdSDK";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../authContext";
+import SnackBar from "../components/SnackBar";
+import { GlobalContext, showToast } from "../globalContext";
+
+const AdminLoginPage = () => {
+  const schema = yup
+    .object({
+      email: yup.string().email().required(),
+      password: yup.string().required(),
+    })
+    .required();
+
+  const { dispatch, state } = React.useContext(AuthContext, GlobalContext);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    data.role = "admin"
+    //TODO
+    try {
+      let sdk = new MkdSDK();
+      let result = await sdk.login(data)
+      if(result){
+          console.log(result)
+
+          localStorage.setItem("token", JSON.stringify(result?.token));
+          navigate("/admin/dashboard")
+        // showToast(dispatch({
+        //   type: "SNACKBAR",
+        //   payload: { message: "Login Sucessful" },
+        // }), "Login Successful");
+      }
+    } catch (error) {
+      wimdow.alert("Failed")
+    }
+
+    
+  };
+
+  return (
+    <div className="w-full max-w-xs mx-auto">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-8 "
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Email"
+            {...register("email")}
+            className={`"shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.email?.message ? "border-red-500" : ""
+            }`}
+          />
+          <p className="text-red-500 text-xs italic">{errors.email?.message}</p>
+        </div>
+
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="******************"
+            {...register("password")}
+            className={`shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.password?.message ? "border-red-500" : ""
+            }`}
+          />
+          <p className="text-red-500 text-xs italic">
+            {errors.password?.message}
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <input
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            value="Sign In"
+          />
+        </div>
+      </form>
+      {state.globalMessage && <SnackBar/>}
+    </div>
+  );
+};
+
+export default AdminLoginPage;
